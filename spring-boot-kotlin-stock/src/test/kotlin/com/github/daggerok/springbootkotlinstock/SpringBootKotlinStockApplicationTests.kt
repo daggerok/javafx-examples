fc6123240511ca8dba0765a5a27b03d7971b23ed
Maksim Kostromin <daggerok@gmail.com>
2020-01-06 22:56:27 +0200

    add dirty scripts from the dark side of the universe (C) whatthecommit.com

---
+++ spring-boot-kotlin-stock/src/test/kotlin/com/github/daggerok/springbootkotlinstock/SpringBootKotlinStockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.daggerok.springbootkotlinstock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinStockApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
